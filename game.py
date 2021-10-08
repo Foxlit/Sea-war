461e4350c60350dda60aadde0e5b958276acaffc
Foxlit <v.gromov2006@gmail.com>
2021-10-08 23:11:30 +0300

    jnl

--- game.py
+++ game.py
@@ -1,6 +1,20 @@
 import random
 
 
+class ShotChecking(Exception):
+    pass
+
+
+class ShotRepeating(ShotChecking):
+    def __str__(self):
+        return 'Сюда уже стреляли'
+
+
+class InvalidInput:
+    def __str__(self):
+        return 'Ошибка ввода'
+
+
 class Dots:
     def __init__(self, x, y):
         self.x = x
@@ -38,12 +52,13 @@ class Ship:
 
 
 class Field:
-    def __init__(self, size):
+    def __init__(self, size, hidden=False):
         self.size = size
         self.cell = [['0']*size for i in range(size)]
         self.deployed = []
         self.ships = []
-        self.ship_quantity = 7
+        self.ship_dots_quantity = 0
+        self.hidden = hidden
 
     def is_in_field(self, point):
         if point.x in range(self.size) and point.y in range(self.size):
@@ -53,27 +68,26 @@ class Field:
 
     def is_empty(self, point):
         for deployed_dot in self.deployed:
-            if deployed_dot == point:
+            if deployed_dot == Dots(point.x, point.y):
                 return False
             else:
                 return True
         return True
 
-    def check_borders(self, dots):
+    def borders_are_empty(self, dots):
         border = [
             (-1, -1), (-1, 0), (-1, 1),
             (0, -1), (0, 0), (0, 1),
             (1, -1), (1, 0), (1, 1)
         ]
         for dot in dots:
-            if self.is_empty(dot):
-                return False
+            # if self.is_empty(dot):
+            #     return False
             for delta_x, delta_y in border:
-                delta_dot = dot.x + delta_x, dot.y + delta_y
-                empty = self.is_empty(delta_dot)
-                print(f'Check emptyness: {empty}')
-                if not empty:
-                    print('kontur')
+                delta_dot = Dots(dot.x + delta_x, dot.y + delta_y)
+                dot_is_empty = self.is_empty(delta_dot)
+                # print(f'Check border emptyness: {dot_is_empty} in ({delta_dot.x};{delta_dot.y})')
+                if not dot_is_empty:
                     return False
         return True
 
@@ -82,38 +96,46 @@ class Field:
         for deployed_cell in ship_dots:
             if self.is_in_field(deployed_cell):
                 all_dots_in_field = True
-                if self.check_borders(ship_dots):
-                    return False
             else:
                 return False
+
+        if not self.borders_are_empty(ship_dots):
+            return False
+
         for deployed_cell in ship_dots:
-            print(f'row: {deployed_cell.x+1} column: {deployed_cell.y+1}')
+            # print(f'row: {deployed_cell.x+1} column: {deployed_cell.y+1}')
             if all_dots_in_field:
-                self.cell[deployed_cell.x][deployed_cell.y] = '■'
-                self.deployed.append(deployed_cell)
+                if self.cell[deployed_cell.x][deployed_cell.y] == '■':
+                    return False
+                else:
+                    self.cell[deployed_cell.x][deployed_cell.y] = '■'
+                    self.deployed.append(deployed_cell)
+                    self.ship_dots_quantity += 1
         return True
 
     def shot(self, cell):
-        for ship in self.ships:
+        for i in self.deployed:
+            if self.cell[cell.x][cell.y] == 'T' or self.cell[cell.x][cell.y] == 'X':
+                return 'no shot'
             if cell in self.deployed:
-                ship.lives -= 1
+
                 self.cell[cell.x][cell.y] = 'X'
-                if ship.lives == 0:
-                    self.ship_quantity -= 1
-                    print('Корабль уничтожен!')
-                else:
-                    print('Корабль ранен')
+                print('Попадание')
+                self.ship_dots_quantity -= 1
+                return self.ship_dots_quantity
             else:
                 self.cell[cell.x][cell.y] = 'T'
                 print('Мимо')
+                return self.ship_dots_quantity
 
     def __str__(self):
-        res = ""
-        res += "  | 1 | 2 | 3 | 4 | 5 | 6 |"
+        val = ""
+        val += "  | 1 | 2 | 3 | 4 | 5 | 6 |"
         for i, row in enumerate(self.cell):
-            res += f"\n{i + 1} | " + " | ".join(row) + " |"
-
-        return res
+            val += f"\n{i + 1} | " + " | ".join(row) + " |"
+        if self.hidden:
+            val = val.replace("■", "0")
+        return val
 
 
 class Player:
@@ -123,38 +145,47 @@ class Player:
 
     def AI_turn(self, oponent_field):
         self.oponent_field = oponent_field
-        self.make_turn(False)
+        return self.make_turn(False, oponent_field)
 
     def human_turn(self, oponent_field):
         self.oponent_field = oponent_field
-        self.make_turn(True)
+        return self.make_turn(True, oponent_field)
 
-    def make_turn(self, is_human):
+    def make_turn(self, is_human, current_field):
         if is_human:
-            cord_x = input('Введите колонку: ')
-            cord_y = input('Введите строку: ')
+            while True:
+                try:
+                    cord_x = int(input('Введите строку: '))-1
+                    cord_y = int(input('Введите колонку: '))-1
+                    if cord_x > current_field.size-1 or cord_y > current_field.size-1:
+                        print(f'Ошибка ввода! Координаты вне поля')
+                        continue
+                    break
+                except ValueError as e:
+                    print(f'Ошибка ввода! Введите число')
         else:
             cord_x = random.randrange(0, self.oponent_field.size - 1)
             cord_y = random.randrange(0, self.oponent_field.size - 1)
-        return Dots(cord_x, cord_y)
+
+        return current_field.shot(Dots(cord_x, cord_y))
 
 
 class Game:
     def __init__(self, size=6):
         self.size = size
         self.player_field = Field(size)
-        self.AI_field = Field(size)
+        self.AI_field = Field(size, True)
 
-    def random_place(self):
+    def random_place(self, is_hidden=False):
         sizes = [3, 2, 2, 1, 1, 1, 1]
-        field = Field(size=self.size)
+        field = Field(hidden=is_hidden, size=self.size)
         for ship_size in sizes:
             result = False
             attempts = 0
             while not result:
                 attempts += 1
-                print(f'Attempt: {attempts} whith size {ship_size}')
-                if attempts > 10:
+                # print(f'Attempt: {attempts} whith size {ship_size}')
+                if attempts > 5000:
                     return None
                 ship = Ship(ship_size, random.randrange(0, 2),
                             Dots(random.randrange(0, self.size), random.randrange(0, self.size)))
@@ -162,24 +193,56 @@ class Game:
                                         Dots(random.randrange(0, self.size), random.randrange(0, self.size)))
                 # ship_dots = ship.deploy(ship_size, 0, Dots(0, 0))
                 result = field.add_deployed(ship_dots)
-                print(f'Result: {result}')
-            print(f'****************** end attempt ******************\n')
-        print(f'End field\n\n')
+        #         print(f'Result: {result}')
+        #     print(f'****************** end attempt ******************\n')
+        # print(f'End field\n\n')
         return field
 
     def game_process(self):
         player = Player(self.AI_field, True)
         player_field = self.random_place()
         AI_player = Player(self.player_field, False)
-        AI_player_field = self.random_place()
+        AI_player_field = self.random_place(True)
         while True:
-            print('Ход игрока: ')
+
             print(player_field)
             print("\n", "*"*26)
             print(AI_player_field)
-            player.human_turn(AI_player_field)
+
+            print('Ход игрока: ')
+            shot_is_done = False
+            while not shot_is_done:
+                AI_dots_left = player.human_turn(AI_player_field)
+                if AI_dots_left == 'no shot':
+                    print(f'Сюда уже стреляли')
+                    continue
+                else:
+                    break
+
+            if AI_dots_left == 0:
+                print('Победил игрок')
+                break
+
             print('Ход компьютера: ')
-            AI_player.AI_turn(player_field)
+
+            shot_is_done = False
+            attempts = 0
+            while not shot_is_done:
+                if attempts > 5000:
+                    print("Не удалось выполнить выстрел")
+                    break
+                pl_dots_left = AI_player.AI_turn(player_field)
+                if pl_dots_left == 'no shot':
+                    attempts += 1
+                    continue
+                else:
+                    break
+            if pl_dots_left == 0:
+                print('Победил компьютер')
+                break
+
+            # print(f'AI Dots left {AI_dots_left}')
+            # print(f'Pl Dots left {pl_dots_left}')
 
 
 game = Game()
